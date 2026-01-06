86cd4bedcf7215d91d169452b309dbd8bab2da7e
Umar Saleem <umersaleem50@gmail.com>
2026-01-06 14:31:04 +0500

    +mutliform

--- src/types/global.tsx
+++ src/types/global.tsx
@@ -1,7 +1,3 @@
-import type { CombinedCheckoutType } from "@/validators/exercises.validator";
-import type { LucideIcon } from "lucide-react";
-import type { ZodType } from "zod";
-
 export type ProjectUserRoleTypes = "admin" | "owner" | "staff";
 
 export interface UserProfile {
@@ -10,26 +6,4 @@ export interface UserProfile {
   avatar_url: string;
   full_name: string;
   updated_at?: Date;
-}
-
-type FieldKeys = keyof CombinedCheckoutType;
-
-export type FormStep = {
-  title: string;
-  position: number;
-  validationSchema: ZodType<unknown>;
-  component: React.ReactElement;
-  icon: LucideIcon;
-  fields: FieldKeys[];
-};
-
-export interface MultiStepFormContextProps {
-  currentStep: FormStep;
-  currentStepIndex: number;
-  isFirstStep: boolean;
-  isLastStep: boolean;
-  nextStep: () => void;
-  previousStep: () => void;
-  goToStep: (step: number) => void;
-  steps: FormStep[];
 }
