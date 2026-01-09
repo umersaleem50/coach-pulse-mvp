df0abae7baac394818f7bedbd0262147febade0b
Umar Saleem <umersaleem50@gmail.com>
2026-01-09 10:12:21 +0500

    multiform, guide complete, need fix

--- src/features/exercises/CreateExerciseForm.tsx
+++ src/features/exercises/CreateExerciseForm.tsx
@@ -1,3 +1,5 @@
+import MultiStepForm from "@/shared/components/MultiStepForm";
+import { Button } from "@/shared/components/ui/button";
 import type { ExerciseFormStep } from "@/types/exercise-form";
 import {
   exerciseDetailsSchema,
@@ -5,12 +7,15 @@ import {
   exerciseVolumeSchema,
 } from "@/validators/exercises.validator";
 import { Dumbbell, FileVideoCamera, Layers } from "lucide-react";
+import BasicExerciseDetails from "./form-steps/BasicExerciseDetails";
+import ExerciseOtherForm from "./form-steps/ExerciseOtherForm";
+import ExerciseVolumeForm from "./form-steps/ExerciseVolumeForm";
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const exerciseFormSteps: ExerciseFormStep[] = [
   {
     title: "Basic Details",
-    component: <></>,
+    component: <BasicExerciseDetails />,
     position: 1,
     validationSchema: exerciseDetailsSchema,
     fields: ["name", "muscles_group", "gender_preference", "type"],
@@ -18,7 +23,7 @@ export const exerciseFormSteps: ExerciseFormStep[] = [
   },
   {
     title: "Exercise Volume",
-    component: <></>,
+    component: <ExerciseVolumeForm />,
     position: 1,
     validationSchema: exerciseVolumeSchema,
     fields: ["sets", "reps", "breaks", "break_duration"],
@@ -26,7 +31,7 @@ export const exerciseFormSteps: ExerciseFormStep[] = [
   },
   {
     title: "Workout Demo",
-    component: <></>,
+    component: <ExerciseOtherForm />,
     position: 1,
     validationSchema: exerciseOtherDetailsSchema,
     fields: ["video_platform", "video_url", "coach_type"],
@@ -34,6 +39,10 @@ export const exerciseFormSteps: ExerciseFormStep[] = [
   },
 ];
 
-export default function ExerciseForm() {
-  return null;
+export default function CreateExerciseForm() {
+  return (
+    <MultiStepForm steps={exerciseFormSteps} localStorageKey="exercise-form">
+      <Button>Create Exercise</Button>
+    </MultiStepForm>
+  );
 }
--- src/features/exercises/form-steps/BasicExerciseDetails.tsx
+++ src/features/exercises/form-steps/BasicExerciseDetails.tsx
@@ -5,23 +5,18 @@ import {
   FieldDescription,
   FieldGroup,
   FieldLabel,
-  FieldLegend,
   FieldSet,
 } from "@/shared/components/ui/field";
 import { Input } from "@/shared/components/ui/input";
 import { useMultiStepForm } from "@/shared/hooks/use-stepped-form";
 import { CombinedExerciseSchema } from "@/validators/exercises.validator";
-import { useFormContext } from "react-hook-form";
+import { Controller, useFormContext } from "react-hook-form";
 import z from "zod";
 import ExerciseTypeSelect from "../form-inputs/ExerciseTypeSelect";
 
 function BasicExerciseDetails() {
-  const {
-    register,
-    getValues,
-    setError,
-    formState: { errors },
-  } = useFormContext<z.infer<typeof CombinedExerciseSchema>>;
+  const { register, control } =
+    useFormContext<z.infer<typeof CombinedExerciseSchema>>();
 
   const { nextStep } = useMultiStepForm();
 
@@ -29,34 +24,40 @@ function BasicExerciseDetails() {
     nextStep();
   }
   return (
-    <FieldSet>
-      <FieldLegend>Basic Details</FieldLegend>
-      <FieldDescription>
-        Please provide the basic information about the exercise.
-      </FieldDescription>
+    <FieldGroup>
+      <FieldSet>
+        <FieldDescription>
+          Please provide the basic information about the exercise.
+        </FieldDescription>
 
-      <FieldGroup>
         <Field>
           <FieldLabel htmlFor="name">Name</FieldLabel>
           <Input
             id="name"
             autoComplete="name"
             placeholder="Enter exercise name, i.e, pushups"
+            {...register("name")}
           />
         </Field>
         <Field>
           <FieldLabel htmlFor="type">Exercise Type</FieldLabel>
-          <ExerciseTypeSelect
+          <Controller
             name="type"
-            options={EXERCISE_TYPES}
-            placeholder="Select a type"
+            control={control}
+            render={({ field }) => (
+              <ExerciseTypeSelect
+                {...field}
+                options={EXERCISE_TYPES}
+                placeholder="Select a type"
+              />
+            )}
           />
         </Field>
-      </FieldGroup>
-      <Field orientation="horizontal">
-        <StepperNextButton onClick={handleOnSubmit} />
-      </Field>
-    </FieldSet>
+        <Field orientation="horizontal">
+          <StepperNextButton onClick={handleOnSubmit} />
+        </Field>
+      </FieldSet>
+    </FieldGroup>
   );
 }
 
