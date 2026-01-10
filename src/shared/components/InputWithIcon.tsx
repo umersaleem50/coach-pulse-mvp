54dfb40204fe03c073bb016a9d91981a56c2dcc7
Umar Saleem <umersaleem50@gmail.com>
2026-01-10 15:38:52 +0500

    old stepper progress

--- src/shared/components/InputWithIcon.tsx
+++ src/shared/components/InputWithIcon.tsx
@@ -1,29 +1,35 @@
-import { useId } from "react";
 import { SearchIcon } from "lucide-react";
-import { Label } from "./ui/label";
+import { useId } from "react";
 import { Input } from "./ui/input";
+import { Label } from "./ui/label";
 
 export interface InputWithIconProps extends React.ComponentProps<"input"> {
   label?: string;
   icon: React.ReactNode;
+  iconPosition?: "start" | "last";
 }
 
-export default function InputWithIcon(props: InputWithIconProps) {
+export default function InputWithIcon({
+  label,
+  icon,
+  iconPosition = "last",
+  ...restProps
+}: InputWithIconProps) {
   const id = useId();
   return (
     <div className="flex flex-col gap-y-1">
-      {props.label ?? <Label htmlFor={id}>{props.label}</Label>}
+      {label ?? <Label htmlFor={id}>{label}</Label>}
       <div className="relative">
-        <Input id={id} className="peer ps-9 pe-9" {...props} />
+        <Input id={id} className="peer ps-9 pe-9" {...restProps} />
         <div className="text-muted-foreground/80 pointer-events-none absolute inset-y-0 start-0 flex items-center justify-center ps-3 peer-disabled:opacity-50">
-          <SearchIcon size={16} />
+          {icon && iconPosition === "start" ? icon : <SearchIcon size={16} />}
         </div>
         <button
           className="text-muted-foreground/80 hover:text-foreground focus-visible:border-ring focus-visible:ring-ring/50 absolute inset-y-0 end-0 flex h-full w-9 items-center justify-center rounded-e-md transition-[color,box-shadow] outline-none focus:z-10 focus-visible:ring-[3px] disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50"
           aria-label="Submit search"
           type="submit"
         >
-          {props.icon}
+          {icon && iconPosition === "last" ? icon : null}
         </button>
       </div>
     </div>
